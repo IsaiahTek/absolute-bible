0e66df6e3e649f9ecf9dd55c947108d24aaf6961
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-03-15 17:35:28 +0100

    merged

--- src/adapters.ts
+++ src/adapters.ts
@@ -3,13 +3,16 @@ import EN_ESV from "./bible-version-adapters/ESV_TO_APP_STANDARD"
 import { BibleJSON_FROM_XML } from "./bible-version-adapters/XML_TO_STANDARD"
 import NG_YORUBA from "./bible-version-adapters/YORUBA_TO_APP_STANDARD"
 
-export const adapters:{[x:string]:any} = {"asv":EN_ASV, "bible_esv":EN_ESV, "yoruba-bible":NG_YORUBA, "bible_amp":BibleJSON_FROM_XML, "Bible_English_GNB":BibleJSON_FROM_XML, "Bible_English_MSG":BibleJSON_FROM_XML}
+export const adapters:{[x:string]:any} = {"asv":EN_ASV, "bible_esv":BibleJSON_FROM_XML, "yoruba-bible":NG_YORUBA, "bible_amp":BibleJSON_FROM_XML, "Bible_English_GNB":BibleJSON_FROM_XML, "Bible_English_MSG":BibleJSON_FROM_XML, "Bible_English_NLT": BibleJSON_FROM_XML, "Bible_English_NLV":BibleJSON_FROM_XML}
 
 export const fetchBible = async(selectedVersion:version):Promise<book[]>=>{
   try {
-    let usesAdapter = adapters[selectedVersion.abbreviation]
+    let usesAdapter = Object.keys(adapters).includes(selectedVersion.abbreviation); 
     let isXmlBible = usesAdapter && adapters[selectedVersion.abbreviation] === BibleJSON_FROM_XML
-    let baseUrl = (usesAdapter)?"./bible_versions/":"./bible_versions/bible-master/json/";
+    let baseUrl = (usesAdapter)?(isXmlBible?"./bible_versions/bible-master/xml/":"./bible_versions/"):"./bible_versions/bible-master/json/";
+    
+    console.log("VERSION address: ", `${baseUrl+selectedVersion.abbreviation}${isXmlBible?".xml":".json"}`, " Uses Adapter: ", usesAdapter);
+    
     let z = await fetch(`${baseUrl+selectedVersion.abbreviation}${isXmlBible?".xml":".json"}`).then(async(response)=>{
       return isXmlBible?await response.text():await response.json();
     })
