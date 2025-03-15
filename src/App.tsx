610bf426654df0580a2f41dd106861625a4b1927
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-03-15 18:12:22 +0100

    updating ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import {
   HashRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
+  useNavigate
 } from 'react-router-dom';
-import { ThemeProvider, createTheme} from '@mui/material'
+import { Box, Button, ThemeProvider, createTheme} from '@mui/material'
 import React from 'react'
 import Home from './Home';
 import { HistoriesPage } from './pages/TabHistory';
 import { SearchPage } from './pages/SearchPage';
+import { AppMenu } from './pages/components';
+import { Backspace } from '@mui/icons-material';
+
+function DynamicTopMenu(){
+  const currentPageName = window.location.pathname;
+  if(currentPageName !== "/"){
+    return (
+      <Box sx={{backgroundColor:"white", display:"flex", paddingLeft:2, alignItems:"center", overflowX:"auto", marginBottom:1}}>
+        <AppMenu />
+        <Box sx={{backgroundColor:"white", paddingY:.8, position:"fixed", zIndex:5, top:0, left:60, width:"90vw"}}>
+            <Button size="small" onClick={()=>window.history.back()}><Backspace fontSize="small" sx={{marginRight:1}} /> Back</Button>
+        </Box>
+    </Box>
+    )
+  }else{
+    return (<AppMenu />);
+  }
+}
 
 export default function App() {  
   const theme = React.useMemo(()=>{
@@ -33,6 +52,8 @@ export default function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
+      
+        <DynamicTopMenu />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/histories' element={<HistoriesPage />} />
